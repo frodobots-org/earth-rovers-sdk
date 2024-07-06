064e78004674d8ad09b5bf3c177dbed192ae2f5f
Santiago Pravisani <santiagopravisani@gmail.com>
2024-07-06 12:16:34 +0900

    Revert changes

--- static/basicVideoCall.js
+++ static/basicVideoCall.js
@@ -1,105 +1,301 @@
+/*
+ *  These procedures use Agora Video Call SDK for Web to enable local and remote
+ *  users to join and leave a Video Call channel managed by Agora Platform.
+ */
+
+/*
+ *  Create an {@link https://docs.agora.io/en/Video/API%20Reference/web_ng/interfaces/iagorartcclient.html|AgoraRTCClient} instance.
+ *
+ * @param {string} mode - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/clientconfig.html#mode| streaming algorithm} used by Agora SDK.
+ * @param  {string} codec - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/clientconfig.html#codec| client codec} used by the browser.
+ */
+var client;
+
+/*
+ * Clear the video and audio tracks used by `client` on initiation.
+ */
+var localTracks = {
+  videoTrack: null,
+  audioTrack: null
+};
+
+/*
+ * On initiation no users are connected.
+ */
+var remoteUsers = {};
+
+/*
+ * On initiation. `client` is not attached to any project or channel for any specific user.
+ */
+var options = {
+  appid: null,
+  channel: null,
+  uid: null,
+  token: null
+};
+
+// you can find all the agora preset video profiles here https://docs.agora.io/en/Voice/API%20Reference/web_ng/globals.html#videoencoderconfigurationpreset
+var videoProfiles = [{
+  label: "360p_7",
+  detail: "480×360, 15fps, 320Kbps",
+  value: "360p_7"
+}, {
+  label: "360p_8",
+  detail: "480×360, 30fps, 490Kbps",
+  value: "360p_8"
+}, {
+  label: "480p_1",
+  detail: "640×480, 15fps, 500Kbps",
+  value: "480p_1"
+}, {
+  label: "480p_2",
+  detail: "640×480, 30fps, 1000Kbps",
+  value: "480p_2"
+}, {
+  label: "720p_1",
+  detail: "1280×720, 15fps, 1130Kbps",
+  value: "720p_1"
+}, {
+  label: "720p_2",
+  detail: "1280×720, 30fps, 2000Kbps",
+  value: "720p_2"
+}, {
+  label: "1080p_1",
+  detail: "1920×1080, 15fps, 2080Kbps",
+  value: "1080p_1"
+}, {
+  label: "1080p_2",
+  detail: "1920×1080, 30fps, 3000Kbps",
+  value: "1080p_2"
+}];
+var curVideoProfile;
+AgoraRTC.onAutoplayFailed = () => {
+  alert("click to start autoplay!");
+};
+AgoraRTC.onMicrophoneChanged = async changedDevice => {
+  // When plugging in a device, switch to a device that is newly plugged in.
+  if (changedDevice.state === "ACTIVE") {
+    localTracks.audioTrack.setDevice(changedDevice.device.deviceId);
+    // Switch to an existing device when the current device is unplugged.
+  } else if (changedDevice.device.label === localTracks.audioTrack.getTrackLabel()) {
+    const oldMicrophones = await AgoraRTC.getMicrophones();
+    oldMicrophones[0] && localTracks.audioTrack.setDevice(oldMicrophones[0].deviceId);
+  }
+};
+AgoraRTC.onCameraChanged = async changedDevice => {
+  // When plugging in a device, switch to a device that is newly plugged in.
+  if (changedDevice.state === "ACTIVE") {
+    localTracks.videoTrack.setDevice(changedDevice.device.deviceId);
+    // Switch to an existing device when the current device is unplugged.
+  } else if (changedDevice.device.label === localTracks.videoTrack.getTrackLabel()) {
+    const oldCameras = await AgoraRTC.getCameras();
+    oldCameras[0] && localTracks.videoTrack.setDevice(oldCameras[0].deviceId);
+  }
+};
 async function initDevices() {
+  // if (!localTracks.audioTrack) {
+  //   localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack({
+  //     encoderConfig: "music_standard"
+  //   });
+  // }
+  // if (!localTracks.videoTrack) {
+  //   localTracks.videoTrack = await AgoraRTC.createCameraVideoTrack({
+  //     encoderConfig: curVideoProfile.value
+  //   });
+  // }
+  // get mics
   mics = await AgoraRTC.getMicrophones();
   const audioTrackLabel = localTracks.audioTrack.getTrackLabel();
   currentMic = mics.find(item => item.label === audioTrackLabel);
-  document.querySelector(".mic-input").value = currentMic.label;
-  const micList = document.querySelector(".mic-list");
-  micList.innerHTML = '';
+  $(".mic-input").val(currentMic.label);
+  $(".mic-list").empty();
   mics.forEach(mic => {
-    const micItem = document.createElement('a');
-    micItem.classList.add('dropdown-item');
-    micItem.href = '#';
-    micItem.textContent = mic.label;
-    micList.appendChild(micItem);
+    $(".mic-list").append(`<a class="dropdown-item" href="#">${mic.label}</a>`);
   });
 
+  // get cameras
   cams = await AgoraRTC.getCameras();
   const videoTrackLabel = localTracks.videoTrack.getTrackLabel();
   currentCam = cams.find(item => item.label === videoTrackLabel);
-  document.querySelector(".cam-input").value = currentCam.label;
-  const camList = document.querySelector(".cam-list");
-  camList.innerHTML = '';
+  $(".cam-input").val(currentCam.label);
+  $(".cam-list").empty();
   cams.forEach(cam => {
-    const camItem = document.createElement('a');
-    camItem.classList.add('dropdown-item');
-    camItem.href = '#';
-    camItem.textContent = cam.label;
-    camList.appendChild(camItem);
+    $(".cam-list").append(`<a class="dropdown-item" href="#">${cam.label}</a>`);
   });
 }
-
 async function switchCamera(label) {
   currentCam = cams.find(cam => cam.label === label);
-  document.querySelector(".cam-input").value = currentCam.label;
+  $(".cam-input").val(currentCam.label);
+  // switch device of local video track.
   await localTracks.videoTrack.setDevice(currentCam.deviceId);
 }
-
 async function switchMicrophone(label) {
   currentMic = mics.find(mic => mic.label === label);
-  document.querySelector(".mic-input").value = currentMic.label;
+  $(".mic-input").val(currentMic.label);
+  // switch device of local audio track.
   await localTracks.audioTrack.setDevice(currentMic.deviceId);
 }
-
 function initVideoProfiles() {
   videoProfiles.forEach(profile => {
-    const profileItem = document.createElement('a');
-    profileItem.classList.add('dropdown-item');
-    profileItem.setAttribute('label', profile.label);
-    profileItem.href = '#';
-    profileItem.textContent = `${profile.label}: ${profile.detail}`;
-    document.querySelector(".profile-list").appendChild(profileItem);
+    $(".profile-list").append(`<a class="dropdown-item" label="${profile.label}" href="#">${profile.label}: ${profile.detail}</a>`);
   });
   curVideoProfile = videoProfiles.find(item => item.label == '480p_1');
-  document.querySelector(".profile-input").value = `${curVideoProfile.detail}`;
+  $(".profile-input").val(`${curVideoProfile.detail}`);
 }
-
 async function changeVideoProfile(label) {
   curVideoProfile = videoProfiles.find(profile => profile.label === label);
-  document.querySelector(".profile-input").value = `${curVideoProfile.detail}`;
-  if (localTracks.videoTrack) {
-    await localTracks.videoTrack.setEncoderConfiguration(curVideoProfile.value);
-  }
+  $(".profile-input").val(`${curVideoProfile.detail}`);
+  // change the local video track`s encoder configuration
+  localTracks.videoTrack && (await localTracks.videoTrack.setEncoderConfiguration(curVideoProfile.value));
 }
 
+/*
+ * When this page is called with parameters in the URL, this procedure
+ * attempts to join a Video Call channel using those parameters.
+ */
+$(() => {
+  initVideoProfiles();
+  $(".profile-list").delegate("a", "click", function (e) {
+    changeVideoProfile(this.getAttribute("label"));
+  });
+  var urlParams = new URL(location.href).searchParams;
+  options.appid = urlParams.get("appid");
+  options.channel = urlParams.get("channel");
+  options.token = urlParams.get("token");
+  options.uid = urlParams.get("uid");
+  if (options.appid && options.channel) {
+    $("#uid").val(options.uid);
+    $("#appid").val(options.appid);
+    $("#token").val(options.token);
+    $("#channel").val(options.channel);
+    $("#join-form").submit();
+  }
+});
+
+/*
+ * When a user clicks Join or Leave in the HTML form, this procedure gathers the information
+ * entered in the form and calls join asynchronously. The UI is updated to match the options entered
+ * by the user.
+ */
+$("#join-form").submit(async function (e) {
+  e.preventDefault();
+  $("#join").attr("disabled", true);
+  try {
+    client = AgoraRTC.createClient({
+      mode: "rtc",
+      codec: getCodec()
+    });
+    options.channel = $("#channel").val();
+    options.uid = Number($("#uid").val());
+    options.appid = $("#appid").val();
+    options.token = $("#token").val();
+    await join();
+    if (options.token) {
+      $("#success-alert-with-token").css("display", "block");
+    } else {
+      $("#success-alert a").attr("href", `index.html?appid=${options.appid}&channel=${options.channel}&token=${options.token}`);
+      $("#success-alert").css("display", "block");
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    $("#leave").attr("disabled", false);
+  }
+});
+
+/*
+ * Called when a user clicks Leave in order to exit a channel.
+ */
+$("#leave").click(function (e) {
+  leave();
+});
+$('#agora-collapse').on('show.bs.collapse	', function () {
+  initDevices();
+});
+$(".cam-list").delegate("a", "click", function (e) {
+  switchCamera(this.text);
+});
+$(".mic-list").delegate("a", "click", function (e) {
+  switchMicrophone(this.text);
+});
+
+/*
+ * Join a channel, then create local video and audio tracks and publish them to the channel.
+ */
 async function join() {
+  // Add an event listener to play remote tracks when remote user publishes.
   client.on("user-published", handleUserPublished);
   client.on("user-unpublished", handleUserUnpublished);
   options.uid = await client.join(options.appid, options.channel, options.token || null, options.uid || null);
+
+  // Join the channel.
+  // options.uid = await client.join(options.appid, options.channel, options.token || null, options.uid || null);
+  // if (!localTracks.audioTrack) {
+  //   localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack({
+  //     encoderConfig: "music_standard"
+  //   });
+  // }
+  // if (!localTracks.videoTrack) {
+  //   localTracks.videoTrack = await AgoraRTC.createCameraVideoTrack({
+  //     encoderConfig: curVideoProfile.value
+  //   });
+  // }
+
+  // Play the local video track to the local browser and update the UI with the user ID.
+  // localTracks.videoTrack.play("local-player");
+  // $("#local-player-name").text(`localVideo(${options.uid})`);
+  // $("#joined-setup").css("display", "flex");
+
+  // Publish the local video and audio tracks to the channel.
+  // await client.publish(Object.values(localTracks));
+  // console.log("publish success");
 }
 
+/*
+ * Stop all local and remote tracks then leave the channel.
+ */
 async function leave() {
-  for (const trackName in localTracks) {
-    const track = localTracks[trackName];
+  for (trackName in localTracks) {
+    var track = localTracks[trackName];
     if (track) {
       track.stop();
       track.close();
-      localTracks[trackName] = null;
+      localTracks[trackName] = undefined;
     }
   }
 
+  // Remove remote users and player views.
   remoteUsers = {};
-  document.getElementById("remote-playerlist").innerHTML = "";
+  $("#remote-playerlist").html("");
 
+  // leave the channel
   await client.leave();
-  document.getElementById("local-player-name").textContent = "";
-  document.getElementById("join").disabled = false;
-  document.getElementById("leave").disabled = true;
-  document.getElementById("joined-setup").style.display = "none";
+  $("#local-player-name").text("");
+  $("#join").attr("disabled", false);
+  $("#leave").attr("disabled", true);
+  $("#joined-setup").css("display", "none");
   console.log("client leaves channel success");
 }
 
+/*
+ * Add the local use to a remote channel.
+ *
+ * @param  {IAgoraRTCRemoteUser} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to add.
+ * @param {trackMediaType - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/itrack.html#trackmediatype | media type} to add.
+ */
 async function subscribe(user, mediaType) {
   const uid = user.uid;
+  // subscribe to a remote user
   await client.subscribe(user, mediaType);
   console.log("subscribe success");
   if (mediaType === "video") {
-    const playerWrapper = document.createElement('div');
-    playerWrapper.id = `player-wrapper-${uid}`;
-    playerWrapper.innerHTML = `
-      <p class="player-name">(${uid})</p>
-      <div id="player-${uid}" class="player"></div>
-    `;
-    document.getElementById("remote-playerlist").appendChild(playerWrapper);
+    const player = $(`
+      <div id="player-wrapper-${uid}">
+        <p class="player-name">(${uid})</p>
+        <div id="player-${uid}" class="player"></div>
+      </div>
+    `);
+    $("#remote-playerlist").append(player);
     user.videoTrack.play(`player-${uid}`);
   }
   if (mediaType === "audio") {
@@ -107,28 +303,37 @@ async function subscribe(user, mediaType) {
   }
 }
 
+/*
+ * Add a user who has subscribed to the live channel to the local interface.
+ *
+ * @param  {IAgoraRTCRemoteUser} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to add.
+ * @param {trackMediaType - The {@link https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/itrack.html#trackmediatype | media type} to add.
+ */
 function handleUserPublished(user, mediaType) {
   const id = user.uid;
   remoteUsers[id] = user;
   subscribe(user, mediaType);
 }
 
+/*
+ * Remove the user specified from the channel in the local interface.
+ *
+ * @param  {string} user - The {@link  https://docs.agora.io/en/Voice/API%20Reference/web_ng/interfaces/iagorartcremoteuser.html| remote user} to remove.
+ */
 function handleUserUnpublished(user, mediaType) {
   if (mediaType === "video") {
     const id = user.uid;
     delete remoteUsers[id];
-    const playerWrapper = document.getElementById(`player-wrapper-${id}`);
-    if (playerWrapper) playerWrapper.remove();
+    $(`#player-wrapper-${id}`).remove();
   }
 }
-
 function getCodec() {
-  const radios = document.getElementsByName("radios");
-  let value;
-  radios.forEach(radio => {
-    if (radio.checked) {
-      value = radio.value;
+  var radios = document.getElementsByName("radios");
+  var value;
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      value = radios[i].value;
     }
-  });
+  }
   return value;
 }
