8e317f5d8c62d3dc48ccd16d5bf50b2cf919d20b
Esteban Fuhrmann <esteban.fuhrmann@sanacompany.io>
2024-07-30 17:56:33 -0300

    Map fixes

--- browser_service.py
+++ browser_service.py
@@ -1,6 +1,7 @@
 import os
 from pyppeteer import launch
 
+
 class BrowserService:
     def __init__(self):
         self.browser = None
@@ -9,18 +10,32 @@ class BrowserService:
     async def initialize_browser(self):
         if not self.browser:
             try:
-                executable_path = os.getenv('CHROME_EXECUTABLE_PATH', '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')
+                executable_path = os.getenv(
+                    "CHROME_EXECUTABLE_PATH",
+                    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+                )
                 self.browser = await launch(
                     executablePath=executable_path,
                     headless=True,
-                    args=['--ignore-certificate-errors', '--no-sandbox']
+                    args=[
+                        "--ignore-certificate-errors",
+                        "--no-sandbox",
+                        "--disable-gpu",
+                        "--window-size=1280,800",
+                    ],
                 )
                 self.page = await self.browser.newPage()
-                await self.page.setViewport({'width': 1280, 'height': 800})
-                await self.page.setExtraHTTPHeaders({'Accept-Language': 'en-US,en;q=0.9'})
-                await self.page.goto('http://localhost:8000', {'waitUntil': 'networkidle2'})
-                await self.page.click('#join')
-                await self.page.waitForSelector('video')
+                await self.page.setViewport({"width": 1280, "height": 800})
+                await self.page.setExtraHTTPHeaders(
+                    {"Accept-Language": "en-US,en;q=0.9"}
+                )
+                await self.page.goto(
+                    "http://localhost:8000", {"waitUntil": "networkidle2"}
+                )
+                await self.page.click("#join")
+                await self.page.waitForSelector("video")
+                await self.page.waitForSelector("#map")
+
                 await self.page.waitFor(2000)
             except Exception as e:
                 print(f"Error initializing browser: {e}")
@@ -33,22 +48,27 @@ class BrowserService:
         await self.initialize_browser()
 
         # Take screenshot of the video element
-        video_wrapper = await self.page.querySelector('#player-1000')
-        video_element = await video_wrapper.querySelector('video')
-        await video_element.screenshot({'path': video_output_path})
+        video_wrapper = await self.page.querySelector("#player-1000")
+        video_element = await video_wrapper.querySelector("video")
+        await video_element.screenshot({"path": video_output_path})
 
-        # Take screenshot of the Mapbox map element
-        map_element = await self.page.querySelector('#map')
-        await map_element.screenshot({'path': map_output_path})
+        # Wait for the map element to be fully initialized
+        await self.page.waitForSelector("#map")
+
+        # Take screenshot of the Map element
+        map_element = await self.page.querySelector("#map")
+        await map_element.screenshot({"path": map_output_path})
 
         return video_output_path, map_output_path
 
     async def data(self) -> dict:
         await self.initialize_browser()
 
-        bot_data = await self.page.evaluate('''() => {
+        bot_data = await self.page.evaluate(
+            """() => {
         return window.rtm_data;
-        }''')
+        }"""
+        )
 
         return bot_data
 
---
+++ static/map.js
@@ -0,0 +1,71 @@
+let map;
+let marker;
+
+function initMap() {
+  map = L.map("map").setView([0, 0], 2);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+}
+
+// Define updateMarker globally
+window.updateMarker = function (latitude, longitude) {
+  if (marker) {
+    marker.setLatLng([latitude, longitude]);
+  } else {
+    const mainMarkerEl = document.createElement("div");
+    mainMarkerEl.className = "main-marker";
+    const antenna = document.createElement("div");
+    antenna.className = "antenna";
+    mainMarkerEl.appendChild(antenna);
+
+    marker = L.marker([latitude, longitude], {
+      icon: L.divIcon({ className: "main-marker" }),
+    }).addTo(map);
+  }
+  map.setView([latitude, longitude], 22); // Set view with zoom level 22
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  initMap();
+  console.log(checkpointsList);
+  addCheckpointsToMap();
+});
+
+function addCheckpointsToMap() {
+  checkpointsList.forEach((checkpoint) => {
+    const el = document.createElement("div");
+    el.className = "custom-marker";
+    el.style.backgroundColor = "green";
+    el.style.color = "white";
+    el.style.borderRadius = "50%";
+    el.style.width = "18px";
+    el.style.height = "18px";
+    el.style.display = "flex";
+    el.style.justifyContent = "center";
+    el.style.alignItems = "center";
+
+    const checkpointNumber = document.createElement("div");
+    checkpointNumber.className = "checkpoint-number";
+    checkpointNumber.innerHTML = checkpoint.sequence;
+    checkpointNumber.style.fontSize = "14px";
+    checkpointNumber.style.fontWeight = "bold";
+    el.appendChild(checkpointNumber);
+
+    const marker = L.marker(
+      [parseFloat(checkpoint.latitude), parseFloat(checkpoint.longitude)],
+      {
+        icon: L.divIcon({ className: "custom-marker" }), // Usar la clase personalizada
+      }
+    ).addTo(map);
+
+    const markerElement = marker.getElement();
+    if (markerElement) {
+      markerElement.appendChild(el);
+    }
+    marker.bindPopup(`Checkpoint ${checkpoint.sequence}`);
+  });
+}
+
